c27809acc158d27883dbb3b4d8cc8605918c6551
Matěj Kašpar Jirásek <matej.jirasek@me.com>
2019-08-16 17:08:18 +0200

    Add targets to Package

--- Package.swift
+++ Package.swift
@@ -6,23 +6,30 @@ import PackageDescription
 let package = Package(
     name: "FTTestingKit",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "FTTestingKit",
-            targets: ["FTTestingKit"]),
+            targets: ["FTTestingKit"]
+        ),
+        .library(
+            name: "FTTestingKitPromiseKit",
+            targets: ["FTTestingKitPromiseKit"]
+        )
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/mxcl/PromiseKit.git", from: "6.8.4")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "FTTestingKit",
-            dependencies: []),
+            dependencies: []
+        ),
+        .target(
+            name: "FTTestingKitPromiseKit",
+            dependencies: ["FTTestingKit", "PromiseKit"]
+        ),
         .testTarget(
             name: "FTTestingKitTests",
-            dependencies: ["FTTestingKit"]),
+            dependencies: ["FTTestingKit"]
+        )
     ]
 )
