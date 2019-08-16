1f1aad8032b459dcd0e867ae7d4980f8fcdc0c16
Matěj Kašpar Jirásek <matej.jirasek@me.com>
2019-08-16 21:32:21 +0200

    Use PromiseKit 6.10.0 or higher

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/mxcl/PromiseKit.git", from: "6.8.4")
+        .package(url: "https://github.com/mxcl/PromiseKit.git", from: "6.10.0")
     ],
     targets: [
         .target(
