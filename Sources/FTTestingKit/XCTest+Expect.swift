577317879dc15020188f81b0083e586d3670411d
Matěj Kašpar Jirásek <matej.jirasek@me.com>
2019-10-18 21:09:34 +0200

    Add inline documentation

--- Sources/FTTestingKit/XCTest+Expect.swift
+++ Sources/FTTestingKit/XCTest+Expect.swift
@@ -1,6 +1,12 @@
 import XCTest
 
 public extension XCTestCase {
+    /// Executes new asynchronous test using expectation and encapsulates the operation
+    /// into transaction inside a closure.
+    /// - Parameter seconds: Seconds until the expectations will time-out.
+    /// - Parameter reply: Closure for encapsulating asynchronous tests into transactions,
+    ///                    the parameter of the clousre is callback for returning the result
+    ///                    of the asynchronous operation.
     func expect(
         within seconds: TimeInterval,
         reply: @escaping (@escaping (Result<Void, Error>) -> Void) -> Void
