88c9d461c81fb10e7cae7c7420ed3a658899de7c
Matěj Kašpar Jirásek <matej.jirasek@me.com>
2019-10-19 17:41:16 +0200

    Add inline documentation to remaining public methods

--- Sources/FTTestingKit/Int+Times.swift
+++ Sources/FTTestingKit/Int+Times.swift
@@ -1,11 +1,18 @@
-public extension Int {
-    func times(execute: () -> Void) {
+extension Int {
+    /// Executes supplied closure n-times.
+    /// Handy for time measurement tests.
+    /// - Parameter execute: Closure executed n-times.
+    public func times(execute: () -> Void) {
         for _ in 0..<self {
             execute()
         }
     }
 
-    func times<T>(execute: () -> T) -> [T] {
+    /// Creates array of items by executing supplied
+    /// closure n-times. Handy for generating
+    /// arrays of random values.
+    /// - Parameter execute: Closure returning some item n-times.
+    public func times<T>(execute: () -> T) -> [T] {
         return (0..<self).map { _ in execute() }
     }
 }
